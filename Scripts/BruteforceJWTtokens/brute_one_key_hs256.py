e12592b6372d29e547a0ff267dc0c756d1f0d8ba
vladossstrawberry <vladosstrawberry@gmail.com>
2019-05-24 14:27:31 +0300

    --author=vladosstrawberry

--- Scripts/BruteforceJWTtokens/brute_one_key_hs256.py
+++
@@ -1,8 +0,0 @@
-import sys
-from brute_hs256 import brute_from_file_with_key, print_bruted
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        bruted = brute_from_file_with_key(sys.argv[1], sys.argv[2], True)
-        print_bruted(bruted)
